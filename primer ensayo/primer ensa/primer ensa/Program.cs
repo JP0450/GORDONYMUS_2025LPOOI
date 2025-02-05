67a2334189b50393b296c9f26d96654b868ab2ac
JP0450 <jeremy.perez0980@unaula.edu.co>
2025-02-05 09:25:02 -0500

    add new solution

---
+++ primer ensayo/primer ensa/primer ensa/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace primer_ensa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("oelos");
+            Console.ReadKey();
+
+        }
+    }
+}

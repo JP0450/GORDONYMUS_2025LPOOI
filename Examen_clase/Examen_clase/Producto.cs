a27ecc0cfa981c0425f8cf1425bdc398c8c8c480
JP0450 <jeremy.perez0980@unaula.edu.co>
2025-03-29 18:56:18 -0500

    Add files via upload
    examen con cambios que requirio el profesor

---
+++ Examen_clase/Examen_clase/Producto.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen_clase
+{
+    internal class Producto
+    {
+
+        private string nombre;
+        private double precio;
+        private double stock;
+        private string marca;
+
+        public Producto(string nombre, double precio, int stock, string marca)
+        {
+            if (precio<0)
+            {
+                throw new ArgumentException("No puede ser negativo el precio");
+            }
+            if (stock<0)
+            {
+                throw new ArgumentException("No puede ser negativo el stock");
+            }
+            this.nombre = nombre;
+            this.precio = precio;
+            this.stock = stock;
+            this.marca = marca;
+        }
+        public void Setprecio(double precio)
+        {
+            if (precio<0)
+            {
+                throw new ArgumentException("No puede ser negativo el precio");
+            }
+            this.precio = precio;
+        }
+        public void Setstock(double stock)
+        {
+            if (stock < 0)
+            {
+                throw new ArgumentException("No puede ser negativo el stock");
+            }
+            this.stock = stock;
+        }
+        
+
+        //public string Compra(List<Producto> productos, int opcion)
+        //{
+            
+            //switch (opcion)
+            //{
+            //    case 1:
+            //        foreach (var m in productos)
+            //        {
+            //            Console.WriteLine($"Nombre: {m.nombre}, Precio: {m.precio}, Stock: {m.stock}, Marca: {m.marca}");
+            //        }
+            //        Console.WriteLine("Ingrese su nombre");
+            //        string nombre = Console.ReadLine();
+            //        Console.WriteLine("Ingrese su correo");
+            //        string correo = Console.ReadLine();
+            //        Console.WriteLine("Ingrese su telefono");
+            //        string telefono = Console.ReadLine();
+            //        Clientes cliente = new Clientes(nombre, correo, telefono);
+            //        cliente.RegistrarCompra();
+            //        //bucle para comprar cadad uno de los elementos
+            //        //Console.WriteLine("que va a comprar");
+
+            //        Console.WriteLine("que va a comprar");
+            //        return "";
+                    
+            //    case 2://mostrar productos
+            //        foreach (var m in productos)
+            //        {
+            //            Console.WriteLine($"Nombre: {m.nombre}, Precio: {m.precio}, Stock: {m.stock}, Marca: {m.marca}");
+            //        }
+            //        return "";
+            //    case 3:
+            //        return "";
+            //}
+            //return "";
+           
+        //}
+
+        public static void ViewProduct(List<Producto> productos)
+        {
+            Console.WriteLine("bienvenido al supermercado el tumbis ");
+            Console.WriteLine("1. Comprar");
+            Console.WriteLine("2. Ver productos");
+            Console.WriteLine("3. Salir");
+            int opcion = int.Parse(Console.ReadLine());
+
+            switch (opcion)
+            {
+                case 1:
+
+                    foreach (var m in productos)
+                    {
+                        Console.WriteLine($"Nombre: {m.nombre}, Precio: {m.precio}, Stock: {m.stock}, Marca: {m.marca}");
+                    }
+
+                    Console.WriteLine("Ingrese su nombre");
+                    string nombre = Console.ReadLine();
+                    Console.WriteLine("Ingrese su correo");
+                    string correo = Console.ReadLine();
+                    Console.WriteLine("Ingrese su telefono");
+                    string telefono = Console.ReadLine();
+                    Clientes cliente = new Clientes(nombre, correo, telefono);
+                    cliente.RegistrarCompra();
+                    //bucle para comprar cada uno de los elementos
+                    Console.WriteLine("cuanto va a comprar?");
+                    int cantidad = int.Parse(Console.ReadLine());
+                    double compratotal = 0;
+                    for (int i = 0; i < cantidad; i++)
+                    {
+                        Console.WriteLine("que va a comprar?");
+                        int compra = int.Parse(Console.ReadLine());
+                        if (compra == 1)
+                        {
+                            productos[compra - 1].stock--;
+                            compratotal = productos[compra - 1].precio + compratotal;
+                        }
+                        else if (compra == 2)
+                        {
+                            productos[compra - 1].stock--;
+                            compratotal = productos[compra - 1].precio + compratotal;
+                        }
+                        else if (compra == 3)
+                        {
+                            productos[compra - 1].stock--;
+                            compratotal = productos[compra - 1].precio + compratotal;
+                        }
+                    }
+                    Console.WriteLine($"El total de la compra es {compratotal}");
+
+
+                    break;
+
+                case 2:
+                    foreach (var m in productos)
+                    {
+                        Console.WriteLine($"Nombre: {m.nombre}, Precio: {m.precio}, Stock: {m.stock}, Marca: {m.marca}");
+                    }
+
+
+                    break;
+                case 3:
+                    break;
+
+            }
+
+
+
+
+            //string compra=Producto.Compra(productos, opcion);
+            ////compra.Compra();
+
+            Console.ReadKey();
+
+
+
+        }
+
+
+
+    }
+}
---
+++ Examen_clase/Examen_clase/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+
+namespace Examen_clase
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<Empleado> listEmpleado = new List<Empleado>();
+            List<Producto> productos = new List<Producto>();
+            List<Clientes> clientes = new List<Clientes>();
+
+            //Console.WriteLine("numero de empleados a implementar");
+            //int ne = int.Parse(Console.ReadLine());
+
+            //for (int i = 0; i < ne; i++)
+            //{
+            //    Console.WriteLine("Nombre del empleado");
+            //    string nombre = Console.ReadLine();
+            //    Console.WriteLine("Cargo del empleado");
+            //    string cargo = Console.ReadLine();
+            //    Console.WriteLine("Salario del empleado");
+            //    double salario = double.Parse(Console.ReadLine());
+            //    listEmpleado.Add(new Empleado(nombre, cargo, salario));
+            //}
+
+            //stock de productos actualizable y que clientes puedan comprar
+            listEmpleado.Add(new Empleado("Jorge", "Analista", 2800000));
+            listEmpleado.Add(new Empleado("Anita", "Cajera", 1200000));
+            listEmpleado.Add(new Gerente("toño", "Gerente", 7896098, 20000));
+            productos.Add(new Producto("1.Deshodorante", 1, 15, "Axe"));
+            productos.Add(new Producto("2.Musica", 1, 10, "Orga"));
+            productos.Add(new Producto("3.Camisa", 1, 11, "LV"));
+
+           //Clientes cliente = new Clientes("Sneyder", "Sneyder.marin8119@unaula.edu.co", "3014879875");
+
+            //cliente.RegistrarCompra();
+
+            
+
+            Gerente gerente = new Gerente("Pipe", "Gerente Comercial", 6856098, 500000);
+            Empleado empleado = new Empleado("Paola", "Aseo", 100000);
+            Console.WriteLine($"La diferencia de salario entre la cajera y el gerente es de {gerente.CalcularSalarioTotal() - empleado.CalcularSalarioTotal()}");
+            foreach (var m in listEmpleado)
+            {
+                Console.WriteLine($"El salario de {m.Nombre} es {m.CalcularSalarioTotal()}");
+               // Console.WriteLine($"La diferencia de salario entre la cajera y el gerente es de {gerente.CalcularSalarioTotal() - m.CalcularSalarioTotal()}");
+            }
+            
+
+           
+            Console.ReadKey();
+
+            do //el metodo funciona gracias al static, esto indica que no es necesario inicializar la clase para poder usar el metodo
+            {
+                Producto.ViewProduct(productos);
+
+            } while (true);
+
+
+
+        }
+
+    }
+}
